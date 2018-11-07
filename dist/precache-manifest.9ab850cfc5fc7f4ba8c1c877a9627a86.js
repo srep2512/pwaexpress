b271ad35ed75de0be850328ba6e40f1cb398e960
Felix Brandt <felixbrandt2512@gmail.com>
2018-11-07 17:27:05 +0100

    fixed bar

---
+++ dist/precache-manifest.9ab850cfc5fc7f4ba8c1c877a9627a86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e76a68b19376971e327c",
+    "url": "/js/chunk-vendors.5d4e6aa0.js"
+  },
+  {
+    "revision": "749acd0b0260ec404fad",
+    "url": "/js/app.3c80fc68.js"
+  },
+  {
+    "revision": "c2d530d187e8ee91c3e0",
+    "url": "/js/about.58ebc553.js"
+  },
+  {
+    "revision": "2d246e309a1f5a2d19f8282bbfe6296a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301f94aeeac34f6fa406b2c25a209323",
+    "url": "/img/paypal.301f94ae.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "e76a68b19376971e327c",
+    "url": "/css/chunk-vendors.3a2f3e88.css"
+  },
+  {
+    "revision": "749acd0b0260ec404fad",
+    "url": "/css/app.edae4d29.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4acc20e551c707bc40e2cf2c1b411f6b.js"
+  "/precache-manifest.9ab850cfc5fc7f4ba8c1c877a9627a86.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "eventapp"});
