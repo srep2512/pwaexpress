f22641fce963ab638d9696603ad04def66ce32ab
Felix Brandt <felixbrandt2512@gmail.com>
2018-11-09 15:35:15 +0100

    paypal sandbox

---
+++ dist/precache-manifest.e2dfb501f5c93a0c1aead7aba49ab458.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5536cd3412b90f19207a",
+    "url": "/js/chunk-vendors.875205e6.js"
+  },
+  {
+    "revision": "2c9947ccdcabb6d5e007",
+    "url": "/js/app.8ad9809f.js"
+  },
+  {
+    "revision": "73362790e234123431ae",
+    "url": "/js/about.62b236ff.js"
+  },
+  {
+    "revision": "2f8ad8f0901817ed67ae12d80cc160e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301f94aeeac34f6fa406b2c25a209323",
+    "url": "/img/paypal.301f94ae.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "5536cd3412b90f19207a",
+    "url": "/css/chunk-vendors.8840928a.css"
+  },
+  {
+    "revision": "2c9947ccdcabb6d5e007",
+    "url": "/css/app.095ae999.css"
+  }
+];
