b41b3acf9fe2dc9ed0a15d9b37b03cb21a748a6d
Felix Brandt <felixbrandt2512@gmail.com>
2018-11-26 15:45:49 +0100

    ibm

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-// create the express app
-const app = express()
+const app = express();
 
-// create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-// Catch all routes and redirect to the index file
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
-})
+app.use(staticFileMiddleware);
+app.use(history({
+disableDotRule: true,
+verbose: true
+}));
+app.use(staticFileMiddleware);
 
-// Create default port to serve the app on
-const port = process.env.PORT || 5000
-app.listen(port)
+app.get('/', function (req, res) {
+res.render(path.join(__dirname + '/dist/index.html'));
+});
 
-// Log a feedback that this is actually running
-console.log('Server started on port ' + port)
+var server = app.listen(process.env.PORT || 5000, function () {
+var port = server.address().port;
+console.log("App now running on port", port);
+});
+
