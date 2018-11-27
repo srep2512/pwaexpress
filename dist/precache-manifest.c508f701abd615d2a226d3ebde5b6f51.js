0418d457827ac75418e37da7c7fe56b0c36e6972
Brandt, Felix <Felix.Brandt@ottogroup.com>
2018-11-27 11:15:52 +0100

    pwa try

---
+++ dist/precache-manifest.c508f701abd615d2a226d3ebde5b6f51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5c282175bcdaa13cdc1d",
+    "url": "/js/chunk-vendors.cba6af2e.js"
+  },
+  {
+    "revision": "213bde0b44981f71e330",
+    "url": "/js/app.adfc4407.js"
+  },
+  {
+    "revision": "12af9d68c9cfb055eae0",
+    "url": "/js/about.b58ed086.js"
+  },
+  {
+    "revision": "e175779cd039156141d2786523edabde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "301f94aeeac34f6fa406b2c25a209323",
+    "url": "/img/paypal.301f94ae.png"
+  },
+  {
+    "revision": "4437b7393deb7a1f0af7fbeaf0faa89c",
+    "url": "/img/fot.png"
+  },
+  {
+    "revision": "5c282175bcdaa13cdc1d",
+    "url": "/css/chunk-vendors.029f1313.css"
+  },
+  {
+    "revision": "213bde0b44981f71e330",
+    "url": "/css/app.b2f55144.css"
+  }
+];
